d145ae08a643a9341b1375d641bef8f25254ba9d
Amarnathjj <78036670+Amarnathjj@users.noreply.github.com>
2021-01-29 13:04:54 +0530

    Update sample_config.py

--- config/sample_config.py
+++ config/sample_config.py
@@ -2,24 +2,24 @@
 from pyrogram import filters
 
 # Get these two from https://my.telegram.org
-API_ID = 1234567
-API_HASH = "ab1c23def45fg67890h123i45678j9kl"
+API_ID = 2422809
+API_HASH = "0dd052a44452d0952032fdecf43da30f"
 
 # Get this from @Botfather
-TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
+TOKEN = "1629694619:AAFVR-zeH8dkYVaoxA3myWLt9EG-p0u_Hyk"
 
 # Your MongoDB URI (using a database named "vcpb"), if you don't provide, you can't use the playlist feature and wont see now playing message
-MONGO_DB_URI = ""
+MONGO_DB_URI = "mongodb+srv://vcpb:vcpb@vcpb.dvepo.mongodb.net/vcpb?retryWrites=true&w=majority"
 
 # The IDs of the users which can stream, skip, pause and change volume
 SUDO_USERS = [
-    383407735,
-    951435494,
-    1392620345
+    1383009042,
+    1221693726,
+    900770224
 ]
 
 # The ID of the group where your bot streams
-GROUP = -1001402753006
+GROUP = -1001268409862
 
 # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
 USERS_MUST_JOIN = False
